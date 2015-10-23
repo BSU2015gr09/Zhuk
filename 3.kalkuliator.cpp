cbd1ccf12c0631ddce32f3646a81962d1d554844
Dima Zhuk <dima_zhuk98@mail.ru>
2015-10-23 19:58:36 +0300

    third homework

---
+++ 3.kalkuliator.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>//"Нормальный" калькулятор.
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int b=0,n=1;
+	float rez=0, a=0;
+	char oper, c, e;
+	while (n == 1)
+	{
+		n++;
+		cout << "Введите первое число" << endl;
+		cin >> rez;
+		cout << "Введите операцию" << endl;
+		cin >> oper;
+		switch (oper)
+		{
+		case 'c'://При вводе "c" происходит сброс всех значений.
+			n = 1;
+			break;
+		case 'e'://При вводе"e" происходит выход из программы.
+			n = 3;
+			break;
+		}
+		while (n == 2)
+		{
+			cout << "Введите второе число" << endl;
+			cin >> b;
+			switch (oper)
+			{
+			case '+':
+				rez = rez + b;
+				cout << "Результат = " << rez << endl;
+				break;
+			case '-':
+				rez = rez - b;
+				cout << "Результат = " << rez << endl;
+				break;
+			case '*':
+				rez = rez * b;
+				cout << "Результат = " << rez << endl;
+				break;
+			case '/':
+				switch (int(b))
+				{
+				case 0:
+					cout << "Выражение не имеет смысла.(деление на ноль)" << endl;
+					break;
+				default:
+					rez = rez / b;
+					cout << "Результат = " << rez << endl;
+					break;
+				}
+				break;
+			case 'c':
+				n = 1;
+				break;
+			case 'e':
+				n = 3;
+				break;
+			}
+			switch (int(n))
+			{
+			case 2:
+				cout << "Введите операцию" << endl;
+				cin >> oper;
+				switch (oper)
+				{
+				case 'c':
+					n = 1;
+					break;
+				case 'e':
+					n = 3;
+					break;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	return 0;
+}
---
+++ 3.kosti.cpp
@@ -0,0 +1,275 @@
+﻿# include <iostream>//Игра "Кости"
+#include<cstdlib>
+#include<ctime>
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, f, g, c=3;
+	srand(time(NULL));
+	while (c != 0)
+	{
+		cout << "Введите 1, чтобы играть, или введите 0, чтобы выйти из игры." << endl;
+		cin >> c;
+		switch (int(c))
+		{
+		case 1:
+			cout << "Ваш бросок" << endl;
+			a = 1 + (std::rand() % 6);
+			switch (int(a))
+			{
+			case 1:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 2:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 3:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 4:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 5:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 6:
+				cout << "---------" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "---------" << endl;
+				break;
+			}
+			b = 1 + (std::rand() % 6);
+			switch (int(b))
+			{
+			case 1:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 2:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 3:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 4:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 5:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 6:
+				cout << "---------" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "---------" << endl;
+				break;
+			}
+
+			cout << "Бросок компьютера" << endl;
+			f = 1 + (std::rand() % 6);
+			switch (int(f))
+			{
+			case 1:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 2:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 3:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 4:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 5:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 6:
+				cout << "---------" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "---------" << endl;
+				break;
+			}
+			g = 1 + (std::rand() % 6);
+			switch (int(g))
+			{
+			case 1:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 2:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 3:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 4:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 5:
+				cout << "---------" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|   o   |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "---------" << endl;
+				break;
+			case 6:
+				cout << "---------" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "|       |" << endl;
+				cout << "| o   o |" << endl;
+				cout << "---------" << endl;
+				break;
+			}
+			cout << endl;
+			if (a + b > f + g)
+			{
+				cout << "Вы победили!" << endl;
+			}
+			else if (a + b == f + g)
+			{
+				cout << "Ничья!" << endl;
+			}
+			else
+			{
+				cout << "Победил компьютер!" << endl;
+				cout << endl;
+			}
+			break;
+		case 0:
+			cout << "Конец игры" << endl;
+			break;
+		}
+	}
+	return 0;
+}
