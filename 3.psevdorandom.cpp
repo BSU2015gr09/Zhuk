fbdcc7116f61f68fc01b995b7c2352f9e90a6876
Dima Zhuk <dima_zhuk98@mail.ru>
2015-10-25 16:36:20 +0300

    third homework

---
+++ 3.psevdorandom.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>// Линейный конгруэнтный генератор псевдослучайных чисел
+#include <clocale>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+	int a = 4096, c = 150889, m = 714025;
+	int i = 0;
+	int mas[15];
+	mas[0]=1;
+	cout << "1  = " << mas[0] << endl;
+	for (i = 1; i<15; i++)
+	{
+		mas[i] = (a*mas[i - 1] + c) % m;
+		cout << std::setw(2) << std::left << i + 1 << " = " << mas[i] << endl;
+	}
+	return 0;
+}
