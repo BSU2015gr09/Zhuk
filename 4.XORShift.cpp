8da15775e927fd8f440b5733fb667a8edb09ae5c
Dima Zhuk <dima_zhuk98@mail.ru>
2015-11-01 12:49:20 +0300

    fourth homework

---
+++ 4.XORShift.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+void xorshift(int &x)
+{
+	x = 2;
+	int tmp, a=13, b=15, c=5;
+	tmp = x^( x << a);
+	tmp = tmp^( tmp >> b);
+	x = tmp^ (tmp << c);
+}
+int main()
+{
+	int x = 0;
+	xorshift (x);
+	cout << x << endl;
+	return 0;
+}
