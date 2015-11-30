7512b4d1e09c22dea3ecc80d898c894610c839f8
Dima Zhuk <dima_zhuk98@mail.ru>
2015-11-30 16:39:31 +0300

    Задание a, двумерные массивы

---
+++ 5.a)dv_main.cpp
@@ -0,0 +1,69 @@
+﻿//Задача номер a)двумерные массивы
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int const N = 5;
+
+void initialization(int(&mas)[N][N])//инициализация чисел
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j<N; j++)
+		{
+			mas[i][j] = rand() % 20 - 9;
+		}
+	}
+}
+
+void print(int(&mas)[N][N])//вывод массива
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << mas[i][j] << "\t";
+		}
+		cout << endl << endl;
+	}
+}
+
+void sum_diag(int(&mas)[N][N])//сумма главной диагонали матрици
+{
+	int S = 0;
+	for (int i = 0; i<N; i++)
+	{
+		S += mas[i][i];
+	}
+	cout << "Сумма главной диагонали матрици равна   " << S << endl;
+}
+
+void sum_n_treug(int(&mas)[N][N])//сумма нижнего треугольника матрици
+{
+	int S = 0;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (j < i)
+			{ 
+				S = S + mas[i][j];
+			}
+		}
+	}
+	cout << "Сумма элементов нижнего треугольника равна   " << S << endl;
+}
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+	int mas[N][N];
+	initialization(mas);//инитаализация
+
+	print(mas);//вывод массива
+
+	sum_diag(mas);//сумма главной диагонали матрици
+
+	sum_n_treug(mas);//сумма нижнего треугольника матрици
+}
